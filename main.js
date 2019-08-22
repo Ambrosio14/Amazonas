acf77fe4ca00068e417342aada28cdc7c6135931
Ambrosio14 <54403544+Ambrosio14@users.noreply.github.com>
2019-08-22 14:44:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+    /*$('#animacion').on('click', function(){
+        $(this).animate({
+            height: 50,
+            margin: '23 10'
+         }, 'slow');
+    });*/
+
+    $('#animacion').on('click', function(){
+        $(this).animate({
+            margin: 20
+        }, 'slow');
+    });
+});
